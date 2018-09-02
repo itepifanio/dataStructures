80c775590db8fac9d9517bc6e8f86ec5b607d9ec
Ítalo Epifânio <italoepifaniols@gmail.com>
2018-09-02 11:28:33 -0300

    Melhora a separação dos partidos

--- Linguagem de Programacao/prova1/prova.py
+++ Linguagem de Programacao/prova1/prova.py
@@ -5,19 +5,18 @@ QE = 12684
 # Lendo csv
 df = pd.read_csv("supp/eleicao.csv", sep=";",error_bad_lines=False)
 
-separa_coligacoes = count(df['Partido/Coligação'].str.split('-').count()
+df['Partido/Coligação'] = df['Partido/Coligação'].apply(lambda x : x.split('-')[1] if len(x.split('-')) > 1 else x.split()[0])
 
-df['Partido/Coligação'] = separa_coligacoes.str[1] if separa_coligacoes > 1 else 0
-
-df['Partido/Coligação'] += df['Partido/Coligação'].str.split(' ').str[1]
 # Agrupando por partido e somando os votos
 df = df.groupby(['Partido/Coligação']).sum()
-print(df)
+
 # Introduzindo nova coluna QP =  Quociente partidário
 df["QP"] = (df['Votos']/QE).astype(int)
 
 vagas_a_preencher = total_cadeiras - df["QP"].sum()
 
+# Cria nova coluna para contar as vagas que já foram
+# preenchidas nas vagas residuais a serem distribuidas
 df["Distribuicao"] = 0
 #print(df)
 
@@ -33,8 +32,8 @@ for vaga_residual in range(0, vagas_a_preencher):
             numero_candidato = getattr(row, "Número")
             max = mi
 
-    # Localiza qual candidato deve receber a distribuição            
+    # Localiza qual candidato deve receber a distribuição
     df.loc[df['Número'] == numero_candidato, 'Distribuicao'] = (df['Distribuicao']+1)
 
-
+print(df)
 
