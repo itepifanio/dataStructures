eb1f69d2774b03c267505314ef1a61061a30dfa3
Ítalo Epifâniop <italoepifaniols@gmail.com>
2018-10-22 00:51:38 -0300

    Arrumando print

--- estruturasDados/estruturas/nAryTree/src/main.cpp
+++ estruturasDados/estruturas/nAryTree/src/main.cpp
@@ -5,25 +5,18 @@ int main() {
     // Arvore ternaria com valor 1 na raiz
     NAryTree<int> tree(1, 3);
 
+    // Adição
     tree.root->addChild(2, 3);
-    // Bugado quando digito pra outros valores de array
-    Node<int> b = tree.search(2);
-    b.addChild(3,3);
+    tree.root->children[0]->addChild(3,3);
+    tree.root->children[0]->addChild(4,3);
+    tree.root->addChild(6, 3);
+
     tree.print();
-/*
-    tree.addChild(2);
-    tree.addChild(3);
-    tree.addChild(4);
-*/
-    //tree.addChild(5);
 
-/*
+    // Pesquisa
     Node<int> node = tree.search(2);
+    std::cout << '\n';
+    std::cout << node.data << '\n';
 
-    node.addChild(5, 3);
-*/
-    //std::cout << node.children[0]->data;
-    //std::cout << tree.search(5).data << '\n';
-//    std::cout << node.data << '\n';
     return 0;
 }
