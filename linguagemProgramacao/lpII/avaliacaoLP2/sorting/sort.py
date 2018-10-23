0565438b8960c15c0ceef82509339db16e82f6e4
Ítalo Epifânio <italoepifaniols@gmail.com>
2018-10-23 01:14:37 -0300

    Versal alfa da impressao da arvore

--- linguagemProgramacao/lpII/avaliacaoLP2/sorting/sort.py
+++ linguagemProgramacao/lpII/avaliacaoLP2/sorting/sort.py
@@ -1,11 +1,7 @@
-import locale
-
 from contextlib import redirect_stdout
 
-locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
-
 with open("rotulos.in", encoding="utf-8") as f:
-    ordered = sorted([line.strip() for line in f.readlines()], key=locale.strxfrm)
+    ordered = sorted([line.strip() for line in f.readlines()])
 
 with open("rotulos.out", "w", encoding="utf-8") as f:
     with redirect_stdout(f):
