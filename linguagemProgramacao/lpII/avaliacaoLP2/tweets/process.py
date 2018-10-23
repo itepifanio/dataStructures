577db9cb9bf8d82c92f57d3769f4aee3c64775d6
Ítalo Epifânio <italoepifaniols@gmail.com>
2018-10-23 17:15:12 -0300

    Update process.py

--- linguagemProgramacao/lpII/avaliacaoLP2/tweets/process.py
+++ linguagemProgramacao/lpII/avaliacaoLP2/tweets/process.py
@@ -1,8 +1,6 @@
 from contextlib import redirect_stdout
 import re
 
-regexMail = r'@w+'
-
 with open("tweets.in", encoding="utf-8") as f:
     tweets = [line.strip() for line in f.readlines()]
     tweets.sort()
