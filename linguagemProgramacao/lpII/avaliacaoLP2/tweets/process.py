a91d14f63f9788dec3414069ca3de7a25f6215a9
Ítalo Epifâniop <italoepifaniols@gmail.com>
2018-10-23 17:10:23 -0300

    Resolve regex nos tweets

--- linguagemProgramacao/lpII/avaliacaoLP2/tweets/process.py
+++ linguagemProgramacao/lpII/avaliacaoLP2/tweets/process.py
@@ -6,11 +6,10 @@ regexMail = r'@w+'
 with open("tweets.in", encoding="utf-8") as f:
     tweets = [line.strip() for line in f.readlines()]
     tweets.sort()
-#(@\w+)|(http[s]?\S+)
 
 with open("tweets.out", "w", encoding="utf-8") as f:
-    #with redirect_stdout(f):
-    for t in tweets:
-        regex = list(re.finditer("(RT @\w+)|(http[s]?\S+)", t))
-        if len(regex) > 1 and "RT" in regex[0].group():
-            print("{} {}".format(regex[0].group().split()[1], regex[1].group()))
+    with redirect_stdout(f):
+        for t in tweets:
+            regex = list(re.finditer("(RT @\w+)|(http[s]?\S+)", t))
+            if len(regex) > 1 and "RT" in regex[0].group():
+                print("{} {}".format(regex[0].group().split()[1], regex[1].group()))
