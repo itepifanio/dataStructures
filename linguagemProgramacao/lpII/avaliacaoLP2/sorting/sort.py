70e71188c72d8535567b754e11ba38aa865cda7c
Ítalo Epifânio <italoepifaniols@gmail.com>
2018-10-23 22:57:59 -0300

    Atualiza repositorio

--- linguagemProgramacao/lpII/avaliacaoLP2/sorting/sort.py
+++ linguagemProgramacao/lpII/avaliacaoLP2/sorting/sort.py
@@ -1,7 +1,10 @@
 from contextlib import redirect_stdout
+import locale
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 with open("rotulos.in", encoding="utf-8") as f:
-    ordered = sorted([line.strip() for line in f.readlines()])
+    ordered = sorted([line.strip() for line in f.readlines()], key=locale.strxfrm)
 
 with open("rotulos.out", "w", encoding="utf-8") as f:
     with redirect_stdout(f):
