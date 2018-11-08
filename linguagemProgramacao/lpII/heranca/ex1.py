5e14188dc137e7b0d5a85ddf8050ecb5ff13a2ec
Ítalo Epifânio <italoepifaniols@gmail.com>
2018-11-08 20:45:00 -0300

    Exercicio 1

--- linguagemProgramacao/lpII/heranca/ex1.py
+++ linguagemProgramacao/lpII/heranca/ex1.py
@@ -1,20 +1,46 @@
-
-
 class Ponto2d:
     def __init__(self, x, y):
         self.x = x
-        self.y = y 
-        
+        self.y = y
+
     def __str__(self):
-        return "({}, {})".format(self.x, self.y)
-        
+        return "{}, {}".format(self.x, self.y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
+
 class Ponto3d(Ponto2d):
     def __init__(self, x, y, z):
         super().__init__(x, y)
         self.z = z
-        
+
     def __str__(self):
-        return "({}, {}, {})".format(self.x, self.y, self.z)
-        
-        
-print(Ponto3d("1","2","3"))
+        return "{}, {}".format(Ponto2d.__str__(self), self.z)
+
+    @property
+    def r(self):
+        return self.__r
+
+    @r.setter
+    def r(self, r):
+        self.__r = r
+
+def main():
+    print(Ponto2d("1","2"))
+    print(Ponto3d("1","2","3"))
+
+if __name__ == "__main__":
+    main()
