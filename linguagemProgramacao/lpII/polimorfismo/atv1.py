5720b0bf4ac7e44b6efd642468aa30275001c73c
Ítalo Epifânio <italoepifaniols@gmail.com>
2018-11-16 11:27:36 -0300

    Polimorfismo sendo formado a partir do diagrama UML da questão

--- linguagemProgramacao/lpII/polimorfismo/atv1.py
+++ linguagemProgramacao/lpII/polimorfismo/atv1.py
@@ -1,18 +1,29 @@
-class Viajante:
-    mensagem = ""
+class Viajante:  
     def __init__(self, cls):
         self.cls = cls
     
     def dizerOla(self):
-        print(self.cls.mensagem)
+        self.cls.dizerOla(self)
 
 class Brasileiro(Viajante):
-    mensagem = "Olá"
+    def dizerOla(self):
+        print("Ola")
     
 class Espanhol(Viajante):
-    mensagem = "Hola"
+    def dizerOla(self):
+        print("Hola")
     
 class Americano(Viajante):
-    mensagem = "Hello"
+    def dizerOla(self):
+        print("Hello")
     
-Viajante(Americano).dizerOla()
+class TestandoNacionalidade:
+    def main():
+        v1 = Viajante(Americano).dizerOla()
+        v2 = Viajante(Brasileiro).dizerOla()
+        v3 = Viajante(Espanhol).dizerOla()
+    if __name__ == '__main__':
+        main()
+        
+        
+TestandoNacionalidade()
